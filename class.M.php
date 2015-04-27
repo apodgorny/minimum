bd3ff65a513b4c10ce3df82d77235b74d97ccaeb
Lex <ap.public1@gmail.com>
2015-04-27 00:54:41 -1000

    Reversing prev commit

--- class.M.php
+++ class.M.php
@@ -10,13 +10,17 @@
 		}
 		
 		public static function url($sUrl, $sPort=null) {
-			if (!$sPort) {
-				if (preg_match("|^https?://|", $sUrl)) {
-					$sPort = M::HTTPS_PORT();
-				} else {
-					$sPort = M::HTTP_PORT();
-				}
-			}
+
+			// AP: Not sure why I ever added this, but it causes port 80 being added to https
+			// Talk to me if you must add this back
+			
+			// if (!$sPort) {
+			// 	if (preg_match("|^https://|", $sUrl)) {
+			// 		$sPort = M::HTTPS_PORT();
+			// 	} else {
+			// 		$sPort = M::HTTP_PORT();
+			// 	}
+			// }
 			return Abs::absolutize($sUrl, $sPort);
 		}
 		
